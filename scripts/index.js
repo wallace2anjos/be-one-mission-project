706c4301a8657e8f615d354428f10936b919e477
Wallace dos Anjos <wallace2anjos@gmail.com>
2020-04-06 13:13:02 -0300

    first commit
    first commit

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+  //M é a library do materialize
+  //Modal é o método
+
+  var items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+
+});
