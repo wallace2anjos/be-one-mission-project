91f7fd898614bf0a630af4cc872af85160ffc8ca
Wallace dos Anjos <wallace2anjos@gmail.com>
2020-04-29 12:24:45 -0300

    Update auth

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,59 +1,58 @@
+// listen for auth status changes
+auth.onAuthStateChanged( user => {
+  console.log(user)
+})
 // singup
-const signupForm = document.querySelector(`#signup-form`);
-signupForm.addEventListener(`submit`, (e) => {
-  e.preventDefault();
+const signupForm = document.querySelector('#signup-form')
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-  //get user info
-  const email = signupForm[`signup-email`].value;
-  const password = signupForm[`signup-password`].value;
+  // get user info
+  const email = signupForm['signup-email'].value
+  const password = signupForm['signup-password'].value
 
-  //sign up the user
+  // sign up the user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-    console.log(`user signed up`);
-    const modal = document.querySelector(`#modal-signup`);
-    M.Modal.getInstance(modal).close();
-    signupForm.reset();
-  });
-});
+    const modal = document.querySelector('#modal-signup')
+    M.Modal.getInstance(modal).close()
+    signupForm.reset()
+  })
+})
 
-/* One time the user has signed up, he is automatically logged in, then we can do his logout.*/
+/* One time the user has signed up, he is automatically logged in, then we can do his logout. */
 // logout
-const logout = document.querySelector(`#logout`);
-logout.addEventListener(`click`,(e) => {
-  e.preventDefault();
+const logout = document.querySelector('#logout')
+logout.addEventListener('click', (e) => {
+  e.preventDefault()
   auth.signOut().then(() => {
-    console.log(`user logged out`);
-  });
-});
+  })
+})
 
 // login
-const loginForm = document.querySelector(`#login-form`);
-loginForm.addEventListener(`submit`, (e) =>{
-  e.preventDefault();
+const loginForm = document.querySelector('#login-form')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-  //get user info
-  const email = loginForm[`login-email`].value;
-  const password = loginForm[`login-password`].value;
+  // get user info
+  const email = loginForm['login-email'].value
+  const password = loginForm['login-password'].value
 
-  auth.signInWithEmailAndPassword(email, password).catch(function(error){
-    //handle errors here
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  auth.signInWithEmailAndPassword(email, password).catch(function (error) {
+    // handle errors here
+    var errorCode = error.code
+    var errorMessage = error.message
 
-    if (errorCode === `auth/wrong-password`){
-      alert(`Wrong Password`);
+    if (errorCode === 'auth/wrong-password') {
+      alert('Wrong Password')
     } else {
-      alert(errorMessage + ` User dont exist`);
+      alert(errorMessage + ' User dont exist')
     }
-  console.log(error);
+    console.log(error)
   }).then(cred => {
-    console.log(cred.user);
-    console.log(`user logged in`);
-    //close the login modal and reset form
-    const modal = document.querySelector(`#modal-login`);
-    M.Modal.getInstance(modal).close();
-    loginForm.reset();
+    // close the login modal and reset form
+    const modal = document.querySelector('#modal-login')
+    M.Modal.getInstance(modal).close()
+    loginForm.reset()
   })
   /*
   auth.signInWithEmailAndPassword(email, password).then(cred => {
@@ -63,5 +62,5 @@ loginForm.addEventListener(`submit`, (e) =>{
     const modal = document.querySelector(`#modal-login`);
     M.Modal.getInstance(modal).close();
     loginForm.reset();
-  });*/
-});
+  }); */
+})
--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,10 @@
 // setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  var modals = document.querySelectorAll('.modal')
+  M.Modal.init(modals)
+  // M é a library do materialize
+  // Modal é o método
 
-  var modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
-  //M é a library do materialize
-  //Modal é o método
-
-  var items = document.querySelectorAll('.collapsible');
-  M.Collapsible.init(items);
-
-});
+  var items = document.querySelectorAll('.collapsible')
+  M.Collapsible.init(items)
+})
