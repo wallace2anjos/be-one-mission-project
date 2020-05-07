8b09316fdd921af5c8d356a68304a0b794bfc707
Wallace dos Anjos <wallace2anjos@gmail.com>
2020-05-07 18:03:33 -0300

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,6 +1,15 @@
+// det data
+db.collection('guides').get().then(snapshot => {
+  console.log(snapshot.docs)
+})
+
 // listen for auth status changes
 auth.onAuthStateChanged( user => {
-  console.log(user)
+  if (user){
+    console.log('user logged in', user)
+  } else {
+    console.log('user logged out')
+  }
 })
 // singup
 const signupForm = document.querySelector('#signup-form')
